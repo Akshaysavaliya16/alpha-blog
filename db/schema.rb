a2d35a47dba4ba2850cba59c2e3ec79f7b884a80
Akshay Savaliya <savaliyaakshay@i.farm>
2021-08-11 09:56:47 +0530

    articles and user relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_115026) do
+ActiveRecord::Schema.define(version: 2021_08_11_040907) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_10_115026) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "sortTitle"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
